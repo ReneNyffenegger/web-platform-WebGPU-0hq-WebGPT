42a3dff849906e87c652130c5bc364397a5fbe27
René Nyffenegger <mail@renenyffenegger.ch>
2024-10-16 00:51:30 +0200

    more logging

--- tokenizer.js
+++ tokenizer.js
@@ -72,12 +72,24 @@ class GPT2Tokenizer extends Tokenizer {
 
     const lines = bpe_file.split("\n");
     console.log(`     lines.length = ${lines.length}`);
+
+ // Turn the lines in vocab.bpe into an array of arrays of two strings:
+ /*
     const bpe_merges = lines.slice(1, lines.length - 1).map((x) => {
       return x.split(/(\s+)/).filter(function (e) {
         return e.trim().length > 0;
       });
     });
+*/
+    const bpe_merges = lines.slice(1, lines.length - 1).map((x) => {
+      return x.split(/\s+/); //.filter(function (e) {
+ //        return e.trim().length > 0;
+      });
+//  });
     console.log(`     bpe_merges.length = ${bpe_merges.length}`);
+    console.log(`     bpe_merges[0] = ${bpe_merges[0]}`);
+    console.log(`     bpe_merges[1] = ${bpe_merges[1]}`);
+    console.log(`     bpe_merges[6] = ${bpe_merges[6]}`);
 
     const byte_encoder = bytes_to_unicode();
     const byte_decoder = {};
@@ -88,6 +100,7 @@ class GPT2Tokenizer extends Tokenizer {
     this.byte_decoder = byte_decoder;
 
     this.bpe_ranks = dictZip(bpe_merges, range(0, bpe_merges.length));
+
     this.cache = new Map();
     this.vocab_size = Object.keys(encoder).length;
   }
@@ -113,7 +126,7 @@ class GPT2Tokenizer extends Tokenizer {
       let bytes_str = utf8_bytes_printable.join("");
 
       const new_tokens = this.bpe(bytes_str)
-        .split(" ")
+//      .split(" ")
         .map((x) => this.encoder[x]);
 
       bpe_tokens = bpe_tokens.concat(new_tokens);
@@ -129,52 +142,99 @@ class GPT2Tokenizer extends Tokenizer {
   }
 
   bpe(token) {
-  // token is a string and a string is returned.
+ // token is a string and an array of strings is returned.
     console.log(`  bpe, token = >${token}< (${token.constructor.name})`);
     if (this.cache.has(token)) {
        console.log(`   token is cached, returning ${this.cache.get(token)} type = ${this.cache.get(token).constructor.name}`);
        return this.cache.get(token);
     }
+
     let word = token.split("");
-    console.log(`   word = `, word);
-    let pairs = get_pairs(word);
-    if (!pairs) return token;
+    console.log('   word = ', word);
+
+    let pairs = get_bigrams(word);
+
+    console.log('   pairs = ', pairs);
+    if (!pairs) {
+       alert('is pairs ever !pairs?'); // Is this alert ever shown?
+       console.log('   !pairs, return token');
+       return token;
+    }
+
     while (true) {
-      const minPairs = {};
+      console.log('   -- next iteration')
+
+   // While we're iterating over each pair,
+   // we store the pair's rank as key and the pair
+   // itself as value in this dictionary:
+      const rank2pair = {};
+
       pairs.forEach(pair => {
+        console.log('   forEach(pair) = ', pair);
         const rank = this.bpe_ranks[pair];
-        minPairs[isNaN(rank) ? 10e10 : rank] = pair;
+        console.log('   rank = ', rank);
+        rank2pair[isNaN(rank) ? 10e10 : rank] = pair;
       });
-      const keys = Object.keys(minPairs).map((x) => parseInt(x));
-      const bigram = minPairs[Math.min(...keys)];
-      if (!Object.hasOwn(this.bpe_ranks, bigram)) break;
-      const first = bigram[0];
-      const second = bigram[1];
+
+
+   // Even though the dictionary's elements were assigned with
+   // integer keys, they seem to be stored with strings.
+      const ranks = Object.keys(rank2pair).map((x) => parseInt(x));
+      console.log('   ranks = ', ranks);
+
+
+   // Find the pair (bigram) with the minimal rank:
+      const bigram_min_rank = rank2pair[Math.min(...ranks)];
+      console.log('   bigram_min_rank = ', bigram_min_rank);
+      if (!Object.hasOwn(this.bpe_ranks, bigram_min_rank)) {
+   //
+   // The bigram with the minimal rank is not found in vocab.bpe,
+   // we're brekaing out of the loop.
+   // TODO, is this not the same as ranks being equal to [10e10] ?
+         console.log(`   !Object.hasOwn ${bigram_min_rank}, breaking out`);
+         break;
+      }
+
+      const first  = bigram_min_rank[0];
+      const second = bigram_min_rank[1];
+      console.log(`   first = ${first}, second = ${second}`);
+
       let new_word = [];
       let i = 0;
       while (i < word.length) {
+        console.log(`   word[${i}] = ${word[i]}`);
         const j = word.indexOf(first, i);
+      
         if (j === -1) {
           new_word = new_word.concat(word.slice(i));
           break;
         }
+
         new_word = new_word.concat(word.slice(i, j));
         i = j;
+
         if (word[i] === first && i < word.length - 1 && word[i + 1] === second) {
           new_word.push(first + second);
           i = i + 2;
-        } else {
+        }
+        else {
           new_word.push(word[i]);
           i = i + 1;
         }
       }
       word = new_word;
       if (word.length === 1) break;
-      else pairs = get_pairs(word);
+      else pairs = get_bigrams(word);
     }
-    word = word.join(" ");
-    this.cache.set(token, word);
-    return word;
+
+    const bpe_ret = word; // word.join(' ');
+//  word = word.join(" ");
+    this.cache.set(token, bpe_ret);
+//  this.cache.set(token, word);
+//  console.log(`   bpe, returning ${word} (${word.constructor.name})`);
+    console.log(`   bpe, returning ${bpe_ret} (${bpe_ret.constructor.name})`);
+//  return word;
+    return bpe_ret;
   }
 }
 
@@ -227,17 +287,20 @@ console.log(`  bytes_to_unicode`);
   return result;
 };
 
-const get_pairs = (word) => {
+const get_bigrams = (grams) => {
 //
-//    word is an array of 'printable characters'
-//    get_pairs returns a set in which each element is an Array(2).
+//    grams is an array of 'printable characters' or already concatenated printable characters
+//    get_bigrams returns a set in which each element is an Array(2).
 //    Each array stores the neighboring printable characters.
 //
-  console.log(`    get_pairs, word = `, word);
+//    TODO: I've renamed get_words to get_bigrams.
+//          Is the change of the name correct?
+//
+  console.log(`    get_bigrams, grams = `, grams);
   const pairs = new Set();
-  let prev_char = word[0];
-  for (let i = 1; i < word.length; i++) {
-    const char = word[i];
+  let prev_char = grams[0];
+  for (let i = 1; i < grams.length; i++) {
+    const char = grams[i];
     pairs.add([prev_char, char]);
     prev_char = char;
   }
